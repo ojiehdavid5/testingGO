20a2c6e46ec577e50577bb667e7cc6d2b5a922f5
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-04-07 17:29:40 +0100

    testing

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "fmt"
+
+
+func main() {
+xs := []float64{1,2,3,4}
+avg := Average(xs)
+fmt.Println(avg)
+}
---
+++ math.go
@@ -0,0 +1,25 @@
+package main
+
+import "testing"
+
+// Average calculates the average of a slice of float64 numbers.
+func Average(xs []float64) float64 {
+    if len(xs) == 0 {
+        return 0 // or you could return an error
+    }
+    
+    total := float64(0)
+    for _, x := range xs {
+        total += x
+    }
+    return total / float64(len(xs))
+}
+
+// TestAverage tests the Average function.
+func TestAverage(t *testing.T) {
+	var v float64
+	v = Average([]float64{1,2})
+	if v != 1.5 {
+	t.Error("Expected 1.5, got ", v)
+	}
+	}
